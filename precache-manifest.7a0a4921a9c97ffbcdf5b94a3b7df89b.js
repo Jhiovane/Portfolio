9b333245815a90f4bfa2448be291a971f86f7c40
Jhiovane Pinheiro <jiovane_alexsander@hotmail.com>
2019-06-17 21:24:41 -0300

    Updates

---
+++ precache-manifest.7a0a4921a9c97ffbcdf5b94a3b7df89b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "077632756f306d943a0da9d13c05f6b1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "77a8c69331f2d1132cd6",
+    "url": "/Portfolio/static/css/main.f173ed24.chunk.css"
+  },
+  {
+    "revision": "decf1ce3948d7d4aff0f",
+    "url": "/Portfolio/static/js/2.8d248648.chunk.js"
+  },
+  {
+    "revision": "77a8c69331f2d1132cd6",
+    "url": "/Portfolio/static/js/main.b708217e.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "3d263a3ba49151f836fae9352e660f2f",
+    "url": "/Portfolio/static/media/geovanna.3d263a3b.PNG"
+  },
+  {
+    "revision": "5fb75589504d2b802c08f1914fade122",
+    "url": "/Portfolio/static/media/logo3.5fb75589.png"
+  },
+  {
+    "revision": "c06d475e960edd84b152fa884add2159",
+    "url": "/Portfolio/static/media/monitora.c06d475e.png"
+  },
+  {
+    "revision": "6bfaa33005811efc023edc700f3093db",
+    "url": "/Portfolio/static/media/perfil.6bfaa330.jpg"
+  },
+  {
+    "revision": "807f0b304d800d688002d8b8c951c99e",
+    "url": "/Portfolio/static/media/pill.807f0b30.png"
+  },
+  {
+    "revision": "861bb6cee7c96bf6f2d7053e9a1a1e87",
+    "url": "/Portfolio/static/media/sgbs.861bb6ce.png"
+  },
+  {
+    "revision": "41fbc0958c145b1c038f499a716fb1db",
+    "url": "/Portfolio/static/media/truco.41fbc095.PNG"
+  }
+]);
